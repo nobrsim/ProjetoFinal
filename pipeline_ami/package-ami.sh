28ba85c4515bbd694187b2005b68ed5265fe69e4
Talyson Cardoso <talysoncardoso@ymail.com>
2021-12-18 22:53:35 +0000

    .

--- pipeline_ami/package-ami.sh
+++ pipeline_ami/package-ami.sh
@@ -8,6 +8,9 @@ VERSAO=$(git describe --tags $(git rev-list --tags --max-count=1))
 RESOURCE_ID=$(terraform output | grep resource_id | awk '{print $2;exit}' | sed -e "s/\",//g")
 
 cd ../terraform_ami
+
+echo "$(pwd)"
+
 terraform init
 export TF_VAR_versao=$VERSAO
 export TF_VAR_resource_id=$RESOURCE_ID
