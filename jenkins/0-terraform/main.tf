93974eeab9fefb8574393bd14c153f29f6ad40ce
Talyson Cardoso <talysoncardoso@ymail.com>
2021-12-18 02:57:46 +0000

    preparando ec2 que rodará o jenkins

--- jenkins/0-terraform/main.tf
+++ jenkins/0-terraform/main.tf
@@ -22,84 +22,9 @@ data "aws_subnet" "selected" {
   id       = each.value
 }
 
-output "teste_subnet" {
-  value       = aws_subnet.selected
-}
-
-
-resource "aws_instance" "jenkins" {
-  subnet_id                   = "subnet-067892e83bd51e039"
-  ami                         = "ami-0e66f5495b4efdd0f"
-  instance_type               = "t2.large"
-  key_name                    = "kp-turma3-talyson-1"
-  associate_public_ip_address = true
-  tags = {
-    Name = "ec2-talyson-jenkins-tf"
-  }
-  vpc_security_group_ids = ["${aws_security_group.jenkins.id}"]
-  root_block_device {
-    encrypted   = true
-    volume_size = 32
-  }
-}
-
-resource "aws_security_group" "jenkins" {
-  name        = "acessos_jenkins-talyson-tf"
-  description = "acessos_jenkins inbound traffic"
-  vpc_id      = data.aws_vpc.selected.id
-
-  ingress = [
-    {
-      description      = "SSH from VPC"
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = null,
-      security_groups : null,
-      self : null
-    },
-    {
-      description      = "SSH from VPC"
-      from_port        = 8080
-      to_port          = 8080
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = null,
-      security_groups : null,
-      self : null
-    },
-  ]
-
-  egress = [
-    {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"],
-      prefix_list_ids  = null,
-      security_groups : null,
-      self : null,
-      description : "Libera dados da rede interna"
-    }
-  ]
-
-  tags = {
-    Name = "jenkins-lab-talyson-tf"
-  }
-}
-
-# terraform refresh para mostrar o ssh
-output "jenkins" {
+output "output_subnetes" {
   value = [
-    "jenkins",
-    "id: ${aws_instance.jenkins.id}",
-    "private: ${aws_instance.jenkins.private_ip}",
-    "public: ${aws_instance.jenkins.public_ip}",
-    "public_dns: ${aws_instance.jenkins.public_dns}",
-    "ssh -i ~/.ssh/kp-turma3-talyson-1.pem ubuntu@${aws_instance.jenkins.public_dns}"
+    for item in data.aws_subnet.selected :
+    "${item.id} - ${item.cidr_block} - ${item.tags.Name}"
   ]
 }
--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -3,21 +3,31 @@
 cd 0-terraform
 terraform init
 terraform fmt
-terraform apply -auto-approve
+terraform apply --auto-approve
+
+echo "Criando variável de ambiente SUBNET_ID"
+SUBNET_ID=$(terraform output | grep aza-pub | awk '{print $1;exit}' | sed 's/"//g')
+
+export TF_VAR_subnet_id=$SUBNET_ID
+
+cd ../1-terraform
+terraform init
+terraform fmt
+terraform apply --auto-approve
 
 echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
 
-echo "[ec2-jenkins]" > ../1-ansible/hosts # cria arquivo
-echo "$(terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../1-ansible/hosts # captura output faz split de espaco e replace de ",
+echo "[ec2-jenkins]" > ../2-ansible/hosts # cria arquivo
+echo "$(terraform output | grep public | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../2-ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
 sleep 20 # 20 segundos
 
-cd ../1-ansible
+cd ../2-ansible
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/.ssh/kp-turma3-talyson-1.pem
 
-cd ../0-terraform
+cd ../1-terraform
 terraform output
 
 echo "Agora somente abrir a URL: http://$(terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"
