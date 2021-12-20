0bfaa2f508f5493defa4ddb127982bbe9a9a1244
Si <si.timackenzie@gmail.com>
2021-12-20 14:32:30 +0000

    altera name

--- pipelineAMI/deploy.sh
+++ pipelineAMI/deploy.sh
@@ -2,13 +2,8 @@ cd pipeline_ami/terraform_prep
 
 echo "$(pwd)"
 
-<<<<<<< HEAD:pipeline_ami/deploy.sh
 terraform init # Comentado temporáriamento pata testes
 #terraform fmt # Comentado temporáriamento pata testes
-=======
-terraform init 
-terraform fmt 
->>>>>>> dc1b8b648e04c70bcdfb1e38c724f5cb003bf694:pipelineAMI/deploy.sh
 terraform apply --auto-approve
 
 echo "Criando variável de ambiente SUBNET_ID"
@@ -21,7 +16,7 @@ cd ../terraform
 echo "$(pwd)"
 
 terraform init 
-terraform fmt 
+#terraform fmt 
 terraform apply --auto-approve
 
 echo "Aguardando criação de maquinas ..."
@@ -35,7 +30,7 @@ sleep 10 # Dá para melhor com um while
 
 cd ../ansible
 
-echo "Executando ansible ::::: [ ansible-playbook -i hosts provision.yml -u ubuntu --private-key /var/lib/jenkins/kp-turma3-talyson-1.pem ]"
+echo "Executando ansible ::::: [ ansible-playbook -i hosts provision.yml -u ubuntu --private-key /var/lib/jenkins/chave-privada.pem ]"
 # ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provision.yml -u ubuntu --private-key ~/.ssh/kp-turma3-talyson-1.pem
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provision.yml -u ubuntu --private-key /var/lib/jenkins/chave-privada.pem
 # ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provision.yml -u ubuntu --private-key /home/ubuntu/chave-privada.pem
