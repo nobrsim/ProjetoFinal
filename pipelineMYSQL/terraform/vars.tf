6e8d46bde667fa45d72562bb5fbc8db5ce259c1b
Talyson Cardoso <talysoncardoso@ymail.com>
2021-12-20 16:16:21 +0000

    commit do vars

---
+++ pipelineMYSQL/terraform/vars.tf
@@ -0,0 +1,7 @@
+variable "subnet_priv" {
+  default     = {
+    subnet_priv_aza = "subnet-0c1026aeb1f1e5c85",
+    subnet_priv_azb = "subnet-0b18c38c40226df02",
+    subnet_priv_azc = "subnet-0ef5293c612f61d26"
+  }
+}
